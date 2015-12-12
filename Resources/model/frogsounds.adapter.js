5df0831ad6689171e16b9b5ba494054da6e0a17d
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-12-12 18:45:03 +0100

    + spectrum

--- Resources/model/frogsounds.adapter.js
+++ Resources/model/frogsounds.adapter.js
@@ -1,16 +1,20 @@
-if (Ti.App.Properties.hasProperty('SPECIES')) {
+if (Ti.App.Properties.hasProperty('SPECIES') || false) {
 	console.log('Info: species from locale storage');
 	var species = JSON.parse(Ti.App.Properties.getString('SPECIES'));
 } else {
 	console.log('Info: species must build');
+	var csv = require('jp.coe.mod.csvparser');
+    var f = csv.getFileToJSON("model/frogsounds.csv");
+    console.log(f);
 	var data = JSON.parse(Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'model', 'frogsounds.json').read().text);
 	var records = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'model', 'recordslist.text').read().text;
 	var species = {};
 	data.forEach(function(line) {
-		if (!species[line[0]])
-			species[line[0]] = [];
-		if (line[33] && line[17] && records.match(new RegExp(line[33], 'gm')))
-			species[line[0]].push({
+		var latin = line[0];
+		if (!species[latin])
+			species[latin] = [];
+		if (line[33] && line[17] && records.match(new RegExp(line[33], 'gm'))) {
+			species[latin].push({
 				locality : line[2],
 				administrative_area : line[3],
 				country : line[4],
@@ -28,17 +32,24 @@ if (Ti.App.Properties.hasProperty('SPECIES')) {
 				duration : line[32],
 				mp3 : 'http://www.tierstimmenarchiv.de/recordings/' + line[33] + '_short.mp3'
 			});
-
+		}
 	});
 	Object.getOwnPropertyNames(species).forEach(function(s) {
 		if (!species[s].length)
 			delete species[s];
 	});
 	Ti.App.Properties.setString('SPECIES', JSON.stringify(species));
+	Ti.UI.createNotification({
+		message : 'Tierstimmenarchiv erfolgtreich importiert.'
+	}).show();
 }
 exports.getAllSpeciesNames = function() {
-	return Object.getOwnPropertyNames(species);
+	return Ti.App.Properties.hasProperty('SPECIES') ? Object.getOwnPropertyNames(species) : [];
+
 };
 exports.getRecordsBySpecies = function(s) {
-	return species[s];
+	if (Ti.App.Properties.hasProperty('SPECIES'))
+		return species[s];
+	else
+	return {};
 };
